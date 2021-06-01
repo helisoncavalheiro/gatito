09485ba2bdb4bbf4598492ac1b52eb99363e347c
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-06-01 20:26:50 -0300

    Formata o preço em real

--- src/telas/Servicos/Item/index.js
+++ src/telas/Servicos/Item/index.js
@@ -5,21 +5,29 @@ import CampoInteiro from "../../../components/CampoInteiro";
 import estilos from "./estilos";
 
 export default function Item({ nome, preco, descricao }) {
-  
   const [quantidade, setQuantidade] = useState(1);
-  
+
   return (
     <>
       <View style={estilos.informacao}>
         <Text style={estilos.nome}>{nome}</Text>
         <Text style={estilos.descricao}>{descricao}</Text>
-        <Text style={estilos.preco}>{preco}</Text>
+        <Text style={estilos.preco}>
+          {Intl.NumberFormat("pt-BR", {
+            style: "currency",
+            currency: "BRL",
+          }).format(preco)}
+        </Text>
       </View>
       <View style={estilos.carrinho}>
         <View>
           <View style={estilos.valor}>
             <Text style={estilos.descricao}>Quantidade: </Text>
-            <CampoInteiro valor={quantidade} acao={setQuantidade} estilos={estilos.quantidade}  />
+            <CampoInteiro
+              valor={quantidade}
+              acao={setQuantidade}
+              estilos={estilos.quantidade}
+            />
           </View>
           <View style={estilos.valor}>
             <Text style={estilos.descricao}>Preço: </Text>
