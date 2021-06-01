981e90988c543ee34ba887a1c70e6b85eda96884
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-06-01 20:22:02 -0300

    Importa o INTL

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import Servicos from './src/telas/Servicos'
+import Servicos from './src/telas/Servicos';
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
 export default function App() {
   return <Servicos/> ;
 }
