85c21b7fbe0219caed1e58551c3b0fe8cc05a3a2
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-06-01 22:24:58 -0300

    Cria as rotas

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import Servicos from './src/telas/Servicos';
 import Carrinho from './src/telas/Carrinho';
 import 'intl';
 import 'intl/locale-data/jsonp/pt-BR';
-
+import Rotas from './src/rotas';
 export default function App() {
-  return <Carrinho/> ;
+  return <Rotas /> ;
 }
