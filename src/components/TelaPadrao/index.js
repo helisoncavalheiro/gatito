6cd4d0d3ef0fa7a8c5ba396449c66e4d5c979068
Helison Cavalheiro <helisson.cavalheiro@gmail.com>
2021-06-01 22:40:37 -0300

    Ajustes de cores das bordas

--- src/components/TelaPadrao/index.js
+++ src/components/TelaPadrao/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { KeyboardAvoidingView, SafeAreaView, StatusBar } from "react-native";
-import estilosGlobal from "../../estilos";
-
+import estilosGlobal, { cores } from "../../estilos";
+import estilos from "./estilos";
 export default function TelaPadrao({ children }) {
   return (
-    <SafeAreaView style={estilosGlobal.preencher}>
-      <StatusBar />
-      <KeyboardAvoidingView
-        style={estilosGlobal.preencher}
-        behavior={Platform.OS == "ios" ? "padding" : "height"}>
-        {children}
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+    <>
+      <SafeAreaView style={estilos.ajusteTela}>
+        <StatusBar backgroundColor={cores.roxo} />
+        <KeyboardAvoidingView
+          style={estilosGlobal.preencher}
+          behavior={Platform.OS == "ios" ? "padding" : "height"}>
+          {children}
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+      <SafeAreaView style={estilos.ajusteTelaBaixo}/>
+    </>
   );
 }
